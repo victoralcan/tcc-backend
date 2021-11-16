248f5faf13cb3aa6a5b429751fce8a12b065ab7e
victoralcan <victoralcan@gmail.com>
2021-11-16 06:25:48 -0300

    :zap: Alterando estado da mesa ao abrir e fechar conta

--- src/services/Bill/CloseBillService.ts
+++ src/services/Bill/CloseBillService.ts
@@ -1,5 +1,6 @@
 import { getCustomRepository } from 'typeorm';
 import BillsRepository from '../../repositories/BillsRepository';
+import TablesRepository from '../../repositories/TablesRepository';
 
 interface IRequestDTO {
   id: string;
@@ -8,12 +9,26 @@ interface IRequestDTO {
 class CloseBillService {
   public async execute({ id }: IRequestDTO): Promise<void> {
     const billsRepository = getCustomRepository(BillsRepository);
+    const tablesRepository = getCustomRepository(TablesRepository);
     await billsRepository.update(
       {
         id,
       },
       {
         end_date: new Date(),
+      },
+    );
+    const updatedBill = await billsRepository.findOne({
+      where: {
+        id,
+      },
+    });
+    await tablesRepository.update(
+      {
+        id: updatedBill?.table_id,
+      },
+      {
+        busy: false,
       },
     );
   }
--- src/services/Bill/CreateBillService.ts
+++ src/services/Bill/CreateBillService.ts
@@ -1,6 +1,7 @@
 import { getCustomRepository } from 'typeorm';
 import BillsRepository from '../../repositories/BillsRepository';
 import Bill from '../../models/Bill';
+import TablesRepository from '../../repositories/TablesRepository';
 
 interface IRequestDTO {
   table_id: string;
@@ -13,10 +14,19 @@ interface IRequestDTO {
 class CreateBillService {
   public async execute(bill: IRequestDTO): Promise<Bill | undefined> {
     const billsRepository = getCustomRepository(BillsRepository);
+    const tablesRepository = getCustomRepository(TablesRepository);
 
     try {
       const newBill = billsRepository.create(bill);
       await billsRepository.save(newBill);
+      await tablesRepository.update(
+        {
+          id: bill.table_id,
+        },
+        {
+          busy: true,
+        },
+      );
       return newBill;
     } catch (e) {
       return undefined;
