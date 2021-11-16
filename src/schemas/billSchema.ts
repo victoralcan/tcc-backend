2aaf614736e2251898b281e4b3396c42b464fcb5
victoralcan <victoralcan@gmail.com>
2021-11-16 14:41:23 -0300

    :zap: Alterando validação de endpoint de bill

--- src/schemas/billSchema.ts
+++ src/schemas/billSchema.ts
@@ -3,7 +3,6 @@ import * as Yup from 'yup';
 export const cadastroSchema = Yup.object().shape({
   table_id: Yup.string().required(),
   start_date: Yup.string().required(),
-  end_date: Yup.string().required(),
   total_value: Yup.number().required(),
   active: Yup.boolean().required(),
 });
@@ -12,7 +11,6 @@ export const updateSchema = Yup.object().shape({
   id: Yup.string().required(),
   table_id: Yup.string().required(),
   start_date: Yup.string().required(),
-  end_date: Yup.string().required(),
   total_value: Yup.number().required(),
   active: Yup.boolean().required(),
 });
